0e5161095ad7dc0140e32aae959eeca1bf9a0d8c
jinmu333 <dreamhunter2333@gmail.com>
2021-09-21 00:28:51 +0800

    feat: update v2

--- awsl/awsl.py
+++ awsl/awsl.py
@@ -2,7 +2,7 @@ import awsl
 import json
 import time
 import logging
-from typing import List
+from typing import Any, List
 import requests
 import threading
 
@@ -13,6 +13,7 @@ from .wb import new_cookie_sub
 
 _logger = logging.getLogger(__name__)
 WB_DATA_URL = "https://weibo.com/ajax/statuses/mymblog?uid={}&page="
+WB_SHOW_URL = "https://weibo.com/ajax/statuses/show?id={}"
 WB_COOKIE = "SUB={}"
 
 
@@ -36,7 +37,7 @@ class WbAwsl(object):
             try:
                 self.update_db(sql_tools, wbdata)
             except Exception as e:
-                _logger.error(e)
+                _logger.exception(e)
         sql_tools.close()
         threading.Timer(self.ttl_time, self.run).start()
 
@@ -53,14 +54,14 @@ class WbAwsl(object):
 
     def select_max_id(self) -> int:
         res = SqlTools(self.dbpath).auto_fetchone(
-            "SELECT max(id) FROM awsl_log"
+            "SELECT max(id) FROM awsl_mblog"
         )
         return res[0] if res and res[0] else 0
 
     def new_cookie_sub(self):
         self.cookie_sub = new_cookie_sub()
 
-    def wb_get(self, url) -> List:
+    def wb_get(self, url) -> Any:
         try:
             res = requests.get(url=url, headers=self.headers)
             if res.headers.get("Content-Type") == "text/html":
@@ -68,14 +69,16 @@ class WbAwsl(object):
                 t.start()
                 t.join()
                 return self.wb_get(url)
-            return res.json()["data"]["list"]
+            return res.json()
         except Exception as e:
             _logger.exception(e)
-            return []
+            return None
 
     def get_wbdata(self, max_id: int) -> dict:
         for page in range(1, self.max_page):
-            for wbdata in self.wb_get(url=self.url + str(page)):
+            wbdatas = self.wb_get(url=self.url + str(page))
+            wbdatas = wbdatas.get("data", {}).get("list", []) if wbdatas else []
+            for wbdata in wbdatas:
                 if wbdata["id"] <= max_id:
                     break
                 # TODO: 正则是不是更好
@@ -85,19 +88,35 @@ class WbAwsl(object):
             time.sleep(10)
 
     def update_db(self, sql_tools: SqlTools, wbdata: dict) -> None:
-        if not wbdata.get("retweeted_status", {}).get("pic_infos"):
+        if not wbdata.get("retweeted_status"):
             return
         _logger.info("awsl update db id=%s mblogid=%s" % (wbdata["id"], wbdata["mblogid"]))
         sql_tools.execute_commit(
-            "INSERT INTO awsl_log VALUES ('%s', '%s', '%s')" % (
-                wbdata["id"], wbdata["mblogid"], json.dumps(wbdata["retweeted_status"]["pic_infos"])
+            "INSERT INTO awsl_mblog VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (
+                wbdata["id"], wbdata["mblogid"],
+                wbdata["retweeted_status"]["id"], wbdata["retweeted_status"]["mblogid"],
+                wbdata["retweeted_status"]["user"]["id"],
+                json.dumps(wbdata["retweeted_status"]["user"])
             )
         )
+        re_wbdata = self.wb_get(WB_SHOW_URL.format(wbdata["retweeted_status"]["mblogid"]))
+        pic_infos = re_wbdata.get("pic_infos", {})
+        for sequence, pic_id in enumerate(re_wbdata.get("pic_ids", [])):
+            sql_tools.execute_commit(
+                "INSERT INTO awsl_pic VALUES ('%s', '%s', '%s', '%s')" % (
+                    wbdata["id"], sequence, pic_id, json.dumps(pic_infos[pic_id])
+                )
+            )
 
     def init_db(self) -> None:
         _logger.info("awsl init db")
         SqlTools(self.dbpath).auto_commit("""
-            CREATE TABLE if not exists awsl_log (
-                id int, mblogid text, pic_infos text
-            )
+            CREATE TABLE if not exists awsl_mblog (
+                id int, mblogid text, re_id int, re_mblogid text, re_user_id int, re_user text
+            );
+        """)
+        SqlTools(self.dbpath).auto_commit("""
+            CREATE TABLE if not exists awsl_pic (
+                awsl_id int, sequence int, pic_id text, pic_info text
+            );
         """)
--- awsl/router.py
+++ awsl/router.py
@@ -10,7 +10,7 @@ from .config import settings
 from .tools import SqlTools
 
 _logger = logging.getLogger(__name__)
-WB_URL_PREFIX = "https://weibo.com/{}/"
+WB_URL_PREFIX = "https://weibo.com/{}/{}"
 
 router = APIRouter()
 
@@ -18,32 +18,66 @@ router = APIRouter()
 @router.get("/awsl")
 def route_awsl(limit: Optional[int] = 10, offset: Optional[int] = 0):
     _logger.info("awsl get limit %s offest %s" % (limit, offset))
-    wb_data = SqlTools(settings.dbpath).auto_fetchall(
-        "SELECT * FROM awsl_log order by id desc limit {} offset {}".format(limit, offset)
+    wb_ids = SqlTools(settings.dbpath).auto_fetchall(
+        "SELECT id FROM awsl_mblog order by id desc limit {} offset {}".format(
+            limit, offset
+        )
     )
-    res = []
-    for d_id, mblogid, pic_infos in wb_data:
-        res.extend([pic["mw2000"]["url"] for pic in json.loads(pic_infos).values()])
-    return res
+    wb_ids = ["0"] + [str(wb_id[0]) for wb_id in wb_ids if wb_id]
+    pic_infos = SqlTools(settings.dbpath).auto_fetchall(
+        "SELECT pic_info FROM awsl_pic where awsl_id in ({}) order by awsl_id desc, sequence asc".format(
+            ",".join(wb_ids)
+        )
+    )
+    return [json.loads(pic[0])["mw2000"]["url"] for pic in pic_infos]
 
 
 @router.get("/awsl_count")
-def awsl_count_awsl():
-    _logger.info("awsl get_count")
+def awsl_count():
     res = SqlTools(settings.dbpath).auto_fetchone(
-        "SELECT count(1) FROM awsl_log"
+        "SELECT count(1) FROM awsl_mblog"
     )
+    _logger.info("awsl get_count %s", res)
+    return res[0] if res else 0
+
+
+@router.get("/list")
+def awsl_list(limit: Optional[int] = 10, offset: Optional[int] = 0):
+    _logger.info("list get limit %s offest %s" % (limit, offset))
+    res = SqlTools(settings.dbpath).auto_fetchall(
+        """SELECT re_user_id, re_mblogid, pic_info
+        FROM awsl_pic
+        JOIN awsl_mblog
+        ON awsl_pic.awsl_id=awsl_mblog.id
+        order by awsl_id desc, sequence asc limit {} offset {}
+        """.format(
+            limit, offset
+        )
+    )
+    return [{
+        "wb_url": WB_URL_PREFIX.format(str(re_user_id), re_mblogid),
+        "pic_info": json.loads(pic_info)
+    } for re_user_id, re_mblogid, pic_info in res]
+
+
+@router.get("/list_count")
+def awsl_list_count():
+    res = SqlTools(settings.dbpath).auto_fetchone(
+        "SELECT count(1) FROM awsl_pic"
+    )
+    _logger.info("awsl_list_count %s", res)
     return res[0] if res else 0
 
 
 @router.get("/wbrandom")
 def awsl_wb_random():
     limit = 1
-    offset = random.randint(0, awsl_count_awsl())
-    _logger.info("awsl get limit %s offest %s" % (limit, offset))
-    wb_data = SqlTools(settings.dbpath).auto_fetchall(
-        "SELECT * FROM awsl_log order by id desc limit {} offset {}".format(limit, offset)
+    offset = random.randint(0, awsl_list_count())
+    _logger.info("wbrandom get limit %s offest %s" % (limit, offset))
+    wb_data = SqlTools(settings.dbpath).auto_fetchone(
+        "SELECT re_user_id, re_mblogid FROM awsl_mblog order by id desc limit {} offset {}".format(
+            limit, offset)
     )
-    urls = [WB_URL_PREFIX.format(settings.uid) + mblogid for _, mblogid, _ in wb_data]
-    url = urls[random.randint(0, len(urls) - 1)] if len(urls) > 1 else urls[0]
+    re_user_id, re_mblogid = wb_data
+    url = WB_URL_PREFIX.format(str(re_user_id), re_mblogid)
     return RedirectResponse(url)
